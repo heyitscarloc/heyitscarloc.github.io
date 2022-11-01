b413cb6c443b1faf3b8e3c1c3220eb90d61baf75
paintvvitch <115509773+paintvvitch@users.noreply.github.com>
2022-11-01 13:51:01 -0400

    added javascript onlick

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-hello world
+
+function toggleCheckboxText() {
+  if(event.target.checked){
+  event.target.parentElement.style.textDecoration= "line-through";
+  }else{
+    event.target.parentElement.style.textDecoration= "none";
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("hide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
